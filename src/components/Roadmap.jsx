78af3b2504eed261f148d46985a13ceced049044
resident7709 <resident7709@gmail.com>
2024-03-05 08:42:38 +0300

    Add Pricing section

---
+++ src/components/Roadmap.jsx
@@ -0,0 +1,13 @@
+import { roadmap } from '../constants';
+import { Gradient } from './design/Roadmap';
+import { check2, grid, loading1 } from '../assets';
+import Button from './Button';
+import Heading from './Heading';
+import Section from './Section';
+import Tagline from './Tagline';
+
+const Roadmap = () => {
+  return <Section>Roadmap</Section>;
+};
+
+export default Roadmap;
