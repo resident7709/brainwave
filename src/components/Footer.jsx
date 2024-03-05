121b3cbe413d59de3f2d1f241bb79d9bcc4fdf0e
resident7709 <resident7709@gmail.com>
2024-03-05 18:49:27 +0300

    Add Roadmap section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Section from './Section';
+import { socials } from '../constants';
+
+const Footer = () => {
+  return (
+    <Section crosses className='!px-0 !py-10'>
+      Footer
+    </Section>
+  );
+};
+
+export default Footer;
